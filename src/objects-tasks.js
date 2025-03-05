a209c7c44f23c5533d055137a299c485c2652bb6
Alina <lnrzhkv3367337@gmail.com>
2025-03-05 17:32:07 +0300

    fix: fix task problems

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -162,13 +162,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-  this.width = width;
-  this.height = height;
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
-
-Rectangle.prototype.getArea = function () {
-  return this.width * this.height;
-};
 
 /**
  * Returns the JSON representation of specified object
@@ -266,18 +267,8 @@ function sortCitiesArray(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(array, keySelector, valueSelector) {
-  return array.reduce((map, item) => {
-    const key = keySelector(item);
-    const value = valueSelector(item);
-
-    if (!map.has(key)) {
-      map.set(key, []);
-    }
-    map.get(key).push(value);
-
-    return map;
-  }, new Map());
+function group(/* array, keySelector, valueSelector */) {
+  throw new Error('Not implemented');
 }
 
 /**
